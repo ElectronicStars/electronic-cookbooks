75635e13eb25cfe86fc4ecff0d3e6715b5bbfb5f
Mikodes <miguel.lopez.hi@gmail.com>
2014-07-18 18:01:47 +0200

    bower with angular recipe

--- electronicstars-site/recipes/bower.rb
+++ electronicstars-site/recipes/bower.rb
@@ -1,16 +1,13 @@
 
-include_attribute 'nodejs'
-# defines node.js and npm version configuration
-default.nodejs.version = '0.10.0'
-default.nodejs.npm = '1.2.14'
+include_recipe 'nodejs::install_from_source'
+include_recipe 'nodejs::npm'
 
 # defines npm packages to install globally
 default.admin.application.npm_packages = {
   'grunt-cli' => '0.1.6',
   'bower'     => '0.8.5'
 }
-include_recipe 'nodejs::install_from_source'
-include_recipe 'nodejs::npm'
+
 
 # provisions global npm packages
 node.admin.application.npm_packages.each_pair do |pkg, ver|
