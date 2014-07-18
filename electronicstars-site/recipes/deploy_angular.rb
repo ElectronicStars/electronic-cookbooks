a75b15e7de9ba0307d114c23e5c10f8da78dfb25
Mikodes <miguel.lopez.hi@gmail.com>
2014-07-18 22:40:28 +0200

    bower with angular recipe

--- electronicstars-site/recipes/deploy_angular.rb
+++ electronicstars-site/recipes/deploy_angular.rb
@@ -1,10 +1,7 @@
 
 
  # provisions npm application dependencies
-execute 'npm install' do
-    cwd '/srv/www/admin'
-    command '/usr/local/bin/npm install'
-end
+
 
 # provisions bower application dependencies
 execute 'bower install' do
