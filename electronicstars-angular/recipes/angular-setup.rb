75a1fefbdf061a31875d1b375559afc1fab592b0
Mikodes <miguel.lopez.hi@gmail.com>
2014-08-04 11:46:04 +0200

    added angular repo to cookbooks

--- electronicstars-angular/recipes/angular-setup.rb
+++ electronicstars-angular/recipes/angular-setup.rb
@@ -2,7 +2,10 @@ include_recipe 'nodejs::install_from_source'
 include_recipe 'nodejs::npm'
 
 include_recipe "npm"
+node[:deploy].each do |application, deploy|
 
-npm_package "bower" do
-  action :install
+  npm_package "bower" do
+    action :install
+  end
+
 end
