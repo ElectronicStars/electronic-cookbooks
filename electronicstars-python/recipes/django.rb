b0eaebe9cfe3808cb48b3dcfab1be451f71c044c
Mikodes <miguel.lopez.hi@gmail.com>
2014-08-06 00:14:18 +0200

    added angular repo to cookbooks

--- electronicstars-python/recipes/django.rb
+++ electronicstars-python/recipes/django.rb
@@ -50,7 +50,7 @@ node[:deploy].each do |application, deploy|
   #
   # end
   # include_recipe "supervisor"
-  base_command = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'uwsgi')} --http :8080 --module wsgi"
+  base_command = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'uwsgi')} --http :8000 --module wsgi"
   # supervisor_service application do
   #   command base_command
   #   directory ::File.join(deploy[:deploy_to], "current")
