a3408fee2ae24a78b6715adbdc2dd64b1a617fff
Mikodes <miguel.lopez.hi@gmail.com>
2014-08-05 15:45:38 +0200

    added angular repo to cookbooks

--- electronicstars-python/definitions/django_base.rb
+++ electronicstars-python/definitions/django_base.rb
@@ -32,7 +32,7 @@ define :django_configure do
     node.normal[:deploy][application]["django_celery"] = celery
 
     include_recipe 'supervisor'
-    base_command = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'python')} manage.py runserver"
+    base_command = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'uwsgi')} --http :8080 --module wsgi"
     supervisor_service application do
       action :enable
       environment {}
