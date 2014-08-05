ee4fad502d28f13e769a96918ecadc832dc9b1cf
Mikodes <miguel.lopez.hi@gmail.com>
2014-08-06 00:02:53 +0200

    added angular repo to cookbooks

--- electronicstars-python/recipes/django.rb
+++ electronicstars-python/recipes/django.rb
@@ -36,27 +36,28 @@ node[:deploy].each do |application, deploy|
     app_name application
     run_action [] # Don't run actions here
   end
-  uwsgi_bin = File.join(deploy[:deploy_to], 'shared/env/bin/uwsgi')
-  uwsgi_service application do
-    uwsgi_bin uwsgi_bin
-    home_path ::File.join(deploy[:deploy_to], 'current')
-    host "127.0.0.1"
-    port 8080
-    pid_path "/var/run/uwsgi-app.pid"
-    worker_processes 1
-    uid deploy[:user]
-    gid deploy[:group]
-    app "wsgi:application"
-
-  end
-  # base_command = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'uwsgi')} --http :8080 --module wsgi"
-  # supervisor_service application do
-  #   command base_command
-  #   directory ::File.join(deploy[:deploy_to], "current")
-  #   autostart true
-  #   user deploy[:user]
-  #   action [:enable, :start]
+  # uwsgi_bin = File.join(deploy[:deploy_to], 'shared/env/bin/uwsgi')
+  # uwsgi_service application do
+  #   uwsgi_bin uwsgi_bin
+  #   home_path ::File.join(deploy[:deploy_to], 'current')
+  #   host "127.0.0.1"
+  #   port 8080
+  #   pid_path "/var/run/uwsgi-app.pid"
+  #   worker_processes 1
+  #   uid deploy[:user]
+  #   gid deploy[:group]
+  #   app "wsgi:application"
+  #
   # end
+  include_recipe "supervisor"
+  base_command = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'uwsgi')} --http :8080 --module wsgi"
+  supervisor_service application do
+    command base_command
+    directory ::File.join(deploy[:deploy_to], "current")
+    autostart false
+    user deploy[:user]
+    action :enable
+  end
 
   # supervisor_service application do
   #   action :start
