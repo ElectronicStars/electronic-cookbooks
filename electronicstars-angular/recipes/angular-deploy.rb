3f8351f5a3aaeccbee4acc92f9788464737eb613
Mikodes <miguel.lopez.hi@gmail.com>
2015-02-02 19:38:29 +0100

    added angular repo to cookbooks

--- electronicstars-angular/recipes/angular-deploy.rb
+++ electronicstars-angular/recipes/angular-deploy.rb
@@ -1,9 +1,17 @@
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
+  include_recipe "nodejs::npm"
   execute 'bower install -F' do
     cwd ::File.join(deploy[:deploy_to], 'current')
     user deploy[:user]
     group deploy[:group]
     environment ({'HOME' => '/home/deploy'})
   end
+  execute 'grunt build' do
+    cwd ::File.join(deploy[:deploy_to], 'current')
+    user deploy[:user]
+    group deploy[:group]
+    environment ({'HOME' => '/home/deploy'})
+  end
+
 end
