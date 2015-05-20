aee0457467ea4ce808fb4a60478a2dcb5c5fa639
Mikodes <miguel.lopez.hi@gmail.com>
2015-05-20 14:46:54 +0200

    added angular repo to cookbooks

--- electronicstars-python/recipes/nginx-configure.rb
+++ electronicstars-python/recipes/nginx-configure.rb
@@ -1,5 +1,51 @@
 include_recipe 'nginx'
+application = node[:deploy][application_name]
+if application[:ssl_support]
 
+
+  # certificate
+
+  directory "#{node[:nginx][:dir]}/ssl" do
+    action :create
+    owner "root"
+    group "root"
+    mode 0600
+  end
+
+  template "#{node[:nginx][:dir]}/ssl/#{application[:domains].first}.crt" do
+    cookbook 'nginx'
+    mode '0600'
+    source "ssl.key.erb"
+    variables :key => application[:ssl_certificate]
+    notifies :restart, "service[nginx]"
+    only_if do
+      application[:ssl_support]
+    end
+  end
+
+  template "#{node[:nginx][:dir]}/ssl/#{application[:domains].first}.key" do
+    cookbook 'nginx'
+    mode '0600'
+    source "ssl.key.erb"
+    variables :key => application[:ssl_certificate_key]
+    notifies :restart, "service[nginx]"
+    only_if do
+      application[:ssl_support]
+    end
+  end
+
+  template "#{node[:nginx][:dir]}/ssl/#{application[:domains].first}.ca" do
+    cookbook 'nginx'
+    mode '0600'
+    source "ssl.key.erb"
+    variables :key => application[:ssl_certificate_ca]
+    notifies :restart, "service[nginx]"
+    only_if do
+      application[:ssl_support] && application[:ssl_certificate_ca]
+    end
+  end
+
+end
 template "#{node[:nginx][:dir]}/sites-available/core" do
   source "core.nginx.erb"
   owner "root"
