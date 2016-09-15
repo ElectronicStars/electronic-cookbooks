cb22ee8073e91655d9934fb486d8c9749f1066a2
mike <miguel.lopez.hi@gmail.com>
2016-09-15 13:06:54 +0200

    fixed angular recipe for build

--- electronicstars-angular/recipes/angular-deploy.rb
+++ electronicstars-angular/recipes/angular-deploy.rb
@@ -2,24 +2,12 @@ node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
   include_recipe "nodejs::npm"
-  execute 'npm install -g grunt-cli' do
-    cwd ::File.join(deploy[:deploy_to], 'current')
-    user 'root'
-    group 'root'
-  end
   execute 'npm install ' do
     cwd ::File.join(deploy[:deploy_to], 'current')
     user 'root'
     group 'root'
   end
-
-  execute 'bower install -F' do
-    cwd ::File.join(deploy[:deploy_to], 'current')
-    user deploy[:user]
-    group deploy[:group]
-    environment ({'HOME' => '/home/deploy'})
-  end
-  execute 'grunt build' do
+  execute 'gulp build' do
     cwd ::File.join(deploy[:deploy_to], 'current')
     user deploy[:user]
     group deploy[:group]
