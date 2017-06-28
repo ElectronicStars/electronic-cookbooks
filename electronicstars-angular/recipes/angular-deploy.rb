982325459044468691ce02e795cde97a81b480cd
Mike <miguel.lopez.hi@gmail.com>
2017-06-28 11:06:33 +0200

    changed npm user permission

--- electronicstars-angular/recipes/angular-deploy.rb
+++ electronicstars-angular/recipes/angular-deploy.rb
@@ -4,10 +4,10 @@ node[:deploy].each do |application, deploy|
   include_recipe "nodejs::npm"
   execute 'npm install ' do
     cwd ::File.join(deploy[:deploy_to], 'current')
-    user 'root'
-    group 'root'
+    user deploy[:user]
+    group deploy[:group]
   end
-  execute 'bower install -F --allow-root' do
+  execute 'bower install -F ' do
     cwd ::File.join(deploy[:deploy_to], 'current')
     user deploy[:user]
     group deploy[:group]
