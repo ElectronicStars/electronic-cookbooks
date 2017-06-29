aa4b5d41129f390cace2bb73ef34249c4af54c8a
Mike <miguel.lopez.hi@gmail.com>
2017-06-29 11:45:41 +0200

    modified hb action

--- electronicstars-python/recipes/hb-deploy.rb
+++ electronicstars-python/recipes/hb-deploy.rb
@@ -37,57 +37,22 @@ node[:deploy].each do |application, deploy|
     run_action [] # Don't run actions here
   end
   celery = "celery-#{application}"
-  commandCelery = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'celery')} worker --app=backend.celery"
+  commandCelery = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'celery')} worker --app=backend.celery --loglevel INFO"
   supervisor_service celery do
     directory ::File.join(deploy[:deploy_to], "current")
     command commandCelery
-    user "root"
+    user deploy[:user]
+    group deploy[:group]
     autostart true
     autorestart true
-    # stderr_logfile ::File.join(deploy[:deploy_to], "shared", "log", "error_celery.log")
-    # stdout_logfile ::File.join(deploy[:deploy_to], "shared", "log", "current_celery.log")
-
-    action :enable
-
-  end
-  command = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'python')} backend.py -p csslisten -n dev_server_csgo"
-  supervisor_service application do
-    directory ::File.join(deploy[:deploy_to], "current")
-    command command
-    user "root"
-    autostart true
-    autorestart true
-    # stderr_logfile ::File.join(deploy[:deploy_to], "shared", "log", "error.log")
-    # stdout_logfile ::File.join(deploy[:deploy_to], "shared", "log", "current.log")
-    action :enable
-
-
-  end
-  css = "css-#{application}"
-  command_css = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'python')} backend.py -p csslisten -n dev_server_css"
-  supervisor_service css do
-    directory ::File.join(deploy[:deploy_to], "current")
-    command command_css
-    user "root"
-    autostart true
-    autorestart true
-    # stderr_logfile ::File.join(deploy[:deploy_to], "shared", "log", "error.log")
-    # stdout_logfile ::File.join(deploy[:deploy_to], "shared", "log", "current.log")
     action :enable
   end
-
-
-
+  
 
   supervisor_service celery do
     action :restart
   end
-  supervisor_service application do
-    action :restart
-  end
-  supervisor_service css do
-    action :restart
-  end
+ 
 
 
 end
